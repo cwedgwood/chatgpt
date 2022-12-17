77a5d679db9523bd1f7ec47d5938563a027ab47e
Chris Wedgwood <cw@f00f.org>
2022-12-16 16:36:15 -0800

    "write c++ code for The Game of Life"

---
+++ gol.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int ROWS = 20;
+const int COLS = 20;
+
+// Structure to represent a cell in the grid
+struct Cell {
+  bool isAlive;
+  int numLiveNeighbors;
+};
+
+// Function prototypes
+void initializeGrid(vector<vector<Cell>> &grid);
+void printGrid(const vector<vector<Cell>> &grid);
+void updateGrid(vector<vector<Cell>> &grid);
+
+int main() {
+  // Create the grid of cells
+  vector<vector<Cell>> grid(ROWS, vector<Cell>(COLS));
+
+  // Initialize the grid
+  initializeGrid(grid);
+
+  // Print the initial grid
+  cout << "Initial grid:" << endl;
+  printGrid(grid);
+
+  // Update and print the grid for 10 iterations
+  for (int i = 0; i < 10; i++) {
+    updateGrid(grid);
+    cout << "Updated grid:" << endl;
+    printGrid(grid);
+  }
+
+  return 0;
+}
+
+// Initializes the grid with random values
+void initializeGrid(vector<vector<Cell>> &grid) {
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      grid[i][j].isAlive = rand() % 2;
+      grid[i][j].numLiveNeighbors = 0;
+    }
+  }
+}
+
+// Prints the grid to the console
+void printGrid(const vector<vector<Cell>> &grid) {
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      cout << (grid[i][j].isAlive ? 'X' : '.');
+    }
+    cout << endl;
+  }
+  cout << endl;
+}
+
+// Updates the grid according to the rules of The Game of Life
+void updateGrid(vector<vector<Cell>> &grid) {
+  // First, update the numLiveNeighbors field for each cell
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      int numLiveNeighbors = 0;
+
+      // Check the 8 cells surrounding the current cell
+      for (int k = -1; k <= 1; k++) {
+        for (int l = -1; l <= 1; l++) {
+          // Skip the current cell
+          if (k == 0 && l == 0) continue;
+
+          // Check if the neighboring cell is within the grid boundaries
+          if (i + k >= 0 && i + k < ROWS && j + l >= 0 && j + l < COLS) {
+            if (grid[i + k][j + l].isAlive) numLiveNeighbors++;
+          }
+        }
+      }
+
+      grid[i][j].numLiveNeighbors = numLiveNeighbors;
+    }
+  }
+} // this line was missing
